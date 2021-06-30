b8adb7e36649532b730ff9a94fdfb4d48dacc5d7
william caetano cardenas <william.caetano.cardenas@gmail.com>
2021-06-30 16:00:11 -0300

    define the constants

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -1,11 +1,14 @@
 let gameComplete = false;
 // Define the three constants here
+const name = "unknown";
+const score = 0;
+const items = 0;
 
 // Define the player object here
 let player = {
-    name: "unknown",
-    score: 0,
-    items: 0,
+    name,
+    score,
+    items,
 
     getCurrentScore: function () {
         return this.score
