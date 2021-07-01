8796d91ff7862ad460e17538d25cd71a5876658e
william caetano cardenas <william.caetano.cardenas@gmail.com>
2021-07-01 09:13:53 -0300

    Calculate the total bill Find points for expiry date of the product Set the score of the player

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -237,7 +237,9 @@ const getProduct = (prodList, pId) => {
 
 
 // Complete this function
-const calculateBill = (prod, tBill) => { };
+const calculateBill = (prod, tBill) => {
+    return prod.price + tBill
+};
 
 const findPointsToBill = (roundedTotal) => {
     if (roundedTotal > 10 && roundedTotal <= 100) {
@@ -259,12 +261,27 @@ const findPointsToBill = (roundedTotal) => {
 
 
 // Complete this function
-const findPointsForExpDate = (prod) => { };
+const findPointsForExpDate = (prod) => {
+    if(prod.daysToExpire < 30){
+        return 10
+    }
+    return 0
+};
 
 
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
+
+    player.score = player.score + pointsToBill + pointsForExpDate
+
+    if (prod instanceof MagicProduct) {
+        if (prod.isBonus) {
+            player.addPoints(prod.points)
+        } else {
+            player.deductPoints(prod.points)
+        }
+    }
 };
 
 // Complete this function
